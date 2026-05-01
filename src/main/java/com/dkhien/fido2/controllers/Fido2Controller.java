2a5ab91ba786f88178c6354a4ef89ab2b0fbff00
dkhien <dkhien.work@gmail.com>
2026-05-01 11:31:34 +0700

    init repository

---
+++ src/main/java/com/dkhien/fido2/controllers/Fido2Controller.java
@@ -0,0 +1,10 @@
+package com.dkhien.fido2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/fido2")
+public class Fido2Controller {
+
+}
